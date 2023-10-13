69dc44dadef646a25e1b3a33fa6f6df5fe1345bc
Noahcap73 <140212760+Noahcap73@users.noreply.github.com>
2023-10-12 22:20:29 -0700

    Add files via upload

---
+++ Web-Development/JavaScript/Intermediate JavaScript/Library_Classes.js
@@ -0,0 +1,96 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(newIsCheckedOut) {
+    this._isCheckedOut = newIsCheckedOut;
+  }
+
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  }
+
+  getAverageRating() {
+    let sumRating = this.ratings.reduce(
+      (currentSum, rating) => currentSum + rating
+    );
+    return sumRating / this.ratings.length;
+  }
+
+  addRating(newRating) {
+    this.ratings.push(newRating);
+  }
+}
+
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book(
+  "A Short History of Nearly Everything",
+  "Bill Bryson",
+  544
+);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie("Speed", "Jan de Bont", 116);
+
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+
+speed.addRating(1);
+speed.addRating(3);
+speed.addRating(4);
+
+console.log(speed.getAverageRating());
